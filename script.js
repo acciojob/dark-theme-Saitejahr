aff512d27a36a478ec51176fb252d1b5714da63b
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-03-23 10:00:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const swap = document.getElementById("swap");
-swap.addEventListener("click",toggle);
+let button=document.getElementById('swap');
+let body=document.getElementsByTagName('body')
+let main=document.getElementById("app")
 
-function toggle() {
-	document.getElementById(app).style.backgroundColor = "red";
+function swap(){
+    //body.style.backgroundcolor="dark";
+    // var element = document.body;
+    // element.classList.toggle("dark-mode");
+    if(button.getAttribute("class")==="button_day"){
+    document.body.style.backgroundColor = "dark";
+    button.setAttribute("class", "button_night")
+    main.setAttribute("class","night")
+    }
+    else{
+        document.body.style.backgroundColor = "light";
+        button.setAttribute("class", "button_day")
+        main.setAttribute("class","day") 
+    }
+
+    //console.log("hi")
 }
-
